d7ed17b664138a9fc5ea65a6bd336aa616f1f8a1
ErsonFairmanWeryadi <93958152+ErsonFairmanWeryadi@users.noreply.github.com>
2022-03-07 09:48:04 +0700

    Add files via upload

---
+++ variable_user.php
@@ -0,0 +1,12 @@
+<?php
+$nama = "Erson Fairman Weryadi";
+$umur = 19;
+$berat = 22.4;
+
+echo' Nama = ' . $nama;
+echo' <br/> Umur = ' . $umur . ' Tahun';
+echo' <br/>Berat = ' . $berat . ' Kg';
+
+
+echo"<br/> Hello Apa Kabar $nama";
+?>
